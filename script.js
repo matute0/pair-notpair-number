636863c6687266425f56be21312fd81e8beac891
matute0 <matifernandezescuder@hotmail.com>
2023-03-12 19:48:10 -0300

    Pair and not pair detector

---
+++ script.js
@@ -0,0 +1,8 @@
+function num(x){
+    if (x % 2 === 0){
+        console.log(x + " It's pair");
+    } else {
+        console.log(x + " It's not pair");
+    }
+}
+num(8) //Introduce your number in this line
